278a8b7e7535696e0b59f539db4350ca56147476
Jhonata Antunes <jhonata.antunes@outlook.com>
2018-09-13 17:23:27 -0300

    E-mail attachment

--- service/email_service.py
+++ service/email_service.py
@@ -1,7 +1,6 @@
 import boto3
 from botocore.exceptions import ClientError
 
-
 SENDER = "Monitoramento Inteligente de Ambientes <jhonata.antunes@outlook.com>"
 AWS_REGION = "us-west-2"
 SUBJECT = "Alerta de evento"
@@ -9,48 +8,46 @@ CHARSET = "UTF-8"
 
 
 def send_email(recipient, event, date, camera, frame64):
-    body_text = "Um evento foi detectado\r\n" \
-                "O evento {} foi detectado às {} pela camera {}.\r\n" \
-                "MIA.".format(event, date, camera)
-
     body_html = """
     <html>
     <head></head>
     <body>
       <h1>Um evento foi detectado</h1>
       <p>O evento '{}' foi detectado às {} pela camera {}.</p>
-      <img src="data:image/png;base64,{}" />
       <p>MIA</p>
     </body>
     </html>
-                """.format(event, date, camera, frame64)
+                """.format(event, date, camera)
+
+    data = 'From: {}\n' \
+           'To: {}\n' \
+           'Subject: {}\n' \
+           'MIME-Version: 1.0\n' \
+           'Content-type: Multipart/Mixed;boundary="NextPart"\n\n' \
+           '--NextPart\n' \
+           'Content-Type: text/plain\n\n' \
+           '{}\n\n' \
+           '--NextPart\n' \
+           'Content-Type: image/jpeg; name="photo.jpeg;\n' \
+           'Content-Disposition: attachment;' \
+           'filename="print.jpeg"\n\n' \
+           '{}\n\n' \
+           '--NextPart--'.format(SENDER, recipient, SUBJECT, body_html, frame64)
 
     client = boto3.client('ses', region_name=AWS_REGION)
     try:
-        response = client.send_email(
-            Destination={
-                'ToAddresses': [
-                    recipient,
-                ],
+        response = client.send_raw_email(
+            Destinations=[
+            ],
+            FromArn='',
+            RawMessage={
+                'Data': data
             },
-            Message={
-                'Body': {
-                    'Html': {
-                        'Charset': CHARSET,
-                        'Data': body_html,
-                    },
-                    'Text': {
-                        'Charset': CHARSET,
-                        'Data': body_text,
-                    },
-                },
-                'Subject': {
-                    'Charset': CHARSET,
-                    'Data': SUBJECT,
-                },
-            },
-            Source=SENDER,
+            ReturnPathArn='',
+            Source='',
+            SourceArn='',
         )
+
     except ClientError as e:
         return e.response['Error']['Message']
     else:
